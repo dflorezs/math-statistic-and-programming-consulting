689693352938990760b879d8030644464eba9dbb
Dafcode007 <dafcode07@gmail.com>
2021-09-12 23:19:00 -0500

    updates

---
+++ programming/colombia/unal/challenge5/scripts/modificaciones.sql
@@ -0,0 +1,3 @@
+UPDATE album set numero_canciones = 17 WHERE producto_id = 7;
+UPDATE usuario set nombre = 'Benito	' WHERE documento = 10000001;
+DELETE FROM comentario WHERE producto_id = 5 and documento_usuario = 1895457;
