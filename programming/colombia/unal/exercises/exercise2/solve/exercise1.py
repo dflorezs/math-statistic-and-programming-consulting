ed24b81e18b989e11eb0bb3dcb08f8e021dd7031
dafcode007 <dafcode07@gmail.com>
2021-11-10 12:11:39 -0500

    solve exercises

---
+++ programming/colombia/unal/exercises/exercise2/solve/exercise1.py
@@ -0,0 +1,4 @@
+num = 1
+while(num <= 100):
+    print(f"El cuadrado de {num} es {pow(num, 2)}")
+    num += 1
---
+++ programming/colombia/unal/exercises/exercise2/solve/exercise4.py
@@ -0,0 +1,8 @@
+A = 25
+B = 18.9
+age = 2022
+while A >= B:
+    A += A * 0.02
+    B += B *0.03
+    age += 1
+print(f"La población B sera superior a A en el año {age}")
